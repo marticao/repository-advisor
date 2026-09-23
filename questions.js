5e66fa6b83240da95b5f43e96c2fd98c88a77fee
DataAnalyticsPractices <dapo.cuhk@gmail.com>
2026-09-23 14:13:01 +0800

    Update questions.js
    Updated question wording for Q06.

--- questions.js
+++ questions.js
@@ -72,7 +72,7 @@ const questions = [
   },
   {
     "id": "Q06",
-    "title": "Which of the following apply?",
+    "title": "Which of the following best applies to you?",
     "type": "single",
     "required": true,
     "options": [
